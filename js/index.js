19b3695fd4f62457136b1b6276ec237e2654e2da
SkyJin <skyjin@outlook.com>
2018-06-23 22:05:08 +0800

    Frame_0.9

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-	$(".toggle").click(function(){
-		$(".nav").toggle();
-		
-	})
+$(document).ready(function() {
+	//显示md文章
+	$.get("article_demo.md", function(data){
+	    var converter = new showdown.Converter();
+    	var html = converter.makeHtml(data);
+  		$('.real_article').html(html);	
+	});
+   
+
+    $(".toggle").click(function() {
+        $(".nav").toggle();
+    })
 });
