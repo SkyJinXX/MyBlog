8e9f13fbc6d4344b64cb1e037ac452880afc9029
金石开 <Skyjin@outlook.com>
2018-06-30 21:57:26 +0800

    验证邮箱和获取url值

--- js/index.js
+++ js/index.js
@@ -6,20 +6,33 @@ $(document).ready(function() {
   		$('.real_article').html(html);	
 	});
    
-
+	//显示隐藏侧栏
     $(".toggle").click(function() {
         $(".nav").toggle();
     })
+    //字符串按钮功能
     $("#b1").click(function(){
     	var str = $("#str").val();
 
-    	var regex = /^\w+@\w+\.com$/;
-    	var StringArray = str.match(regex);	
-    	if(StringArray != null)
-    		var substr = StringArray[0].replace(/@\w+/, '@sky')
+    	var regex = /^\w+@(\w+)\.com$/;
+
+    	if(regex.test(str))
+    		var substr = regex.exec(str)[1];
     	else 
     		var substr = "Wrong Email";
 
     	$("#subStr").text(substr);
     })
+    //获取地址？后面的值
+    $("#b2").click(function(){
+    	// var url = window.location.search;
+
+    	// var regex = /k=(\w+)/;
+    	// var subStr = regex.exec(url)[1];
+    	$("#subStr").text(new RegExp("k=(\\w+)").exec(window.location.search)[1]);
+    })
 });
+function keyEnter(e){
+	if(e.keyCode==13)
+		$("#b1").click();
+}
